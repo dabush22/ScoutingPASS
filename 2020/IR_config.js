092eccd0d3a838e686037b0487f0d8d56ba0486f
dabush22 <101060927+dabush22@users.noreply.github.com>
2023-02-08 18:30:11 -0500

    Update IR_config.js

--- 2020/IR_config.js
+++ 2020/IR_config.js
@@ -15,7 +15,7 @@ var config_data = `
      { "name": "Event",
        "code": "e",
        "type": "event",
-       "defaultValue": "2020mokc",
+       "defaultValue": "2023miesc",
        "required": "true",
        "disabled": "true"
      },
