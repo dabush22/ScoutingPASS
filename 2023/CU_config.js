1879dc85006bda38f99f961e74f2797be862bf57
dabush22 <101060927+dabush22@users.noreply.github.com>
2023-02-19 12:00:59 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "MIESC",
+      "defaultValue": "WEEK0",
       "required": "true",
       "disabled": "true"
     },
