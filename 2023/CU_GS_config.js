d14d487ce38850cb26dc0b00494492cdc3768631
dabush22 <101060927+dabush22@users.noreply.github.com>
2023-02-26 15:23:37 -0500

    Update CU_GS_config.js

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "WEEK0",
+      "defaultValue": "MIESC",
       "required": "true",
       "disabled": "false"
     },
