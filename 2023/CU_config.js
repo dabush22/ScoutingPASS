8f2a12f13205778bbe33c549d27dd86f5b4caca7
dabush22 <101060927+dabush22@users.noreply.github.com>
2023-03-06 18:34:01 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "mista",
+      "defaultValue": "2023mista",
       "required": "true",
       "disabled": "true"
     },
