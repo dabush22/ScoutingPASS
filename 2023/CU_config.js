bf66d9ff7311b1563e5e22042a308da988d280c7
dabush22 <101060927+dabush22@users.noreply.github.com>
2023-04-20 07:26:49 -0500

    Update CU_config.js

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -14,7 +14,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2023micmp4",
+      "defaultValue": "2023mil",
       "required": "true"
     },
     { "name": "Match Level",
